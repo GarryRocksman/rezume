d0b21b7d0ab468d4e9dbff59ac25949d845691cf
GarryRocksman <garryrocksmanjs@gmail.com>
2021-07-20 00:42:43 +0300

    team hub wiwth information about team members

---
+++ team/Kirill Deli/js/backend.js
@@ -0,0 +1,13 @@
+
+
+window.onload = function(){
+    myFunction();
+}
+function myFunction() {
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+        keyboard: false
+      })
+      var modalToggle = document.getElementById('exampleModal') // relatedTarget
+myModal.show(modalToggle)
+    myModal.show(modalToggle)
+  }
